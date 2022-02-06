556a33224c50397c48277684fd84d54cea2dbd09
Aruzhant <98511793+Aruzhant@users.noreply.github.com>
2022-02-06 15:17:54 +0600

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let totalSum = 0
+  for (let key in salaries) {
+    let isSalary = typeof salaries[key] === 'number' && Number.isFinite(salaries[key]);
+
+    if (isSalary) {
+      totalSum += salaries[key]
+    }
+  }
+  return totalSum
 }
