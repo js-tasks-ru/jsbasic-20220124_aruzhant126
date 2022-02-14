a72365520f57fa36f6a4d4ed4e6e2e2534d0e585
Aruzhan Tulebaeva <aruzhant126@gmail.com>
2022-02-14 17:08:22 +0600

    Можете подсказать ошибки

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
   // ваш код...
+
+  for (let i = 0; i < friends.lenght; i++) {
+  let li = friends.firstName[i] + friends.lastName[i]
+  let list = document. createElement('ul');
+  list.innerHTML = '<li>${li}</li>' ;
+  list.appendChild(li);
+  }
+  
 }
